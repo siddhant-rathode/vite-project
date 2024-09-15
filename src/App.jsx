15cbdc995b98f049841cae06d35be1b30e5923ac
Siddhant Rathode <siddhantrathode@gmail.com>
2024-09-15 17:30:57 +0530

    Add tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-const app = () => {
-  return(
-    <div>
-      <h1>hello from react boiler plate</h1>
+const App = () => {
+  return (
+    <div className="mt-5 w-[80%] m-auto">
+      <h1 className="mt-5 text-5xl">This is React Vite Boilerplate</h1>
+      <h2 className="mt-5 text-3xl">{import.meta.env.VITE_API_KEY}</h2>
+      <button className="mt-5 px-5 py-2 bg-red-300 rounded ">Explore</button>
     </div>
-  )
-}
+  );
+};
 
-export default app;
+export default App;
